9bd1e1418bdbae32fa57d071f46dd53ba2eaaaa8
Sudin Bhattarai <bhattaraisudin211@gmail.com>
2026-01-27 11:37:17 +0545

    commitment

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    context={}
-    return render(request, 'myapp/home.html', context)
+   return render(request, 'home.html')
